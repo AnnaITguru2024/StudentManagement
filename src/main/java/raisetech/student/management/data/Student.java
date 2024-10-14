7bae0b041ec9c1b12d8b96d23a5e132701ea5d82
AnnaITguru2024 <annetteshilova90@gmail.com>
2024-10-14 20:13:22 +0900

    Createと更新処理のREST化、リファクタリング（Postman実践）

--- src/main/java/raisetech/student/management/data/Student.java
+++ src/main/java/raisetech/student/management/data/Student.java
@@ -2,7 +2,6 @@ package raisetech.student.management.data;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @Getter
 @Setter
--- src/main/java/raisetech/student/management/repository/StudentRepository.java
+++ src/main/java/raisetech/student/management/repository/StudentRepository.java
@@ -7,45 +7,82 @@ import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 import raisetech.student.management.data.Student;
-import raisetech.student.management.data.StudentsCourses;
+import raisetech.student.management.data.StudentCourse;
 
 /**
- *受講生情報を扱うリポジトリ。
- * 全件検索や単一条件での検索、コース情報の検索が行えるクラスです。
+ *受講生テーブルと受講生コース情報テーブルと紐づくRepositoryです。
  */
 @Mapper
 public interface StudentRepository {
 
+  /**
+   * 受講生の全件検索を行います。
+   *
+   * @return 受講生一覧（全件）
+   */
   @Select("SELECT * FROM students")
   List<Student> search();
 
+  /**
+   * 受講生の検索を行います。
+   *
+   * @param id 受講生ID
+   * @return 受講生
+   */
   @Select("SELECT * FROM students WHERE id = #{id}")
   Student searchStudent(String id);
 
+  /**
+   * 受講生のコース情報の全件検索を行います。
+   *
+   * @return 受講生のコース情報（全件）
+   */
   @Select("SELECT * FROM students_courses")
-  List<StudentsCourses> searchStudentsCoursesList();
-
-  @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
-  List<StudentsCourses> searchStudentsCourses(String studentId);
+  List<StudentCourse> searchStudentCourseList();
 
   /**
-   * 新しい受講生情報をデータベースに保存します。
-   * @param student 保存する受講生情報
+   * 受講生IDに紐づく受講生コース情報を検索します。
+   *
+   * @param studentId　受講生ID
+   * @return 受講生IDに紐づく受講生コース情報
+   */
+  @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
+  List<StudentCourse> searchStudentCourse(String studentId);
+
+  /**
+   * 受講生を新規登録します。IDに関しては自動採番を行う。
+   *
+   * @param student 受講生
    */
   @Insert("INSERT INTO students(name, furigana, nickname, email, city, age, gender, remark, isDeleted)"
       + "VALUES(#{name}, #{furigana}, #{nickname}, #{email}, #{city}, #{age}, #{gender}, #{remark}, false)")
   @Options(useGeneratedKeys = true, keyProperty = "id")
   void registerStudent(Student student);
 
+  /**
+   * 受講生コース情報を新規登録します。IDに関しては自動採番を行う。
+   *
+   * @param studentCourse　受講生コース情報
+   */
   @Insert("INSERT INTO students_courses (student_id, course_name, start_date, end_date) "
       + "VALUES (#{studentId}, #{courseName}, #{startDate}, #{endDate})")
   @Options(useGeneratedKeys = true, keyProperty = "id")
-  void registerStudentsCourses(StudentsCourses studentsCourses);
+  void registerStudentCourse(StudentCourse studentCourse);
 
+  /**
+   * 受講生を更新します。
+   *
+   * @param student 受講生
+   */
   @Update("UPDATE students SET name = #{name}, furigana = #{furigana}, nickname = #{nickname}, "
       + "email = #{email}, city = #{city}, age = #{age}, gender = #{gender}, remark = #{remark}, isDeleted = #{isDeleted} WHERE id = #{id}")
   void updateStudent(Student student);
 
+  /**
+   * 受講生コース情報のコース名を更新します。
+   *
+   * @param studentCourse 受講生コース情報
+   */
   @Update("UPDATE students_courses SET course_name = #{courseName} WHERE id = #{id}")
-  void updateStudentsCourses(StudentsCourses studentsCourses);
+  void updateStudentCourse(StudentCourse studentCourse);
 }
